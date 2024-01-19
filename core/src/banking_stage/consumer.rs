64ab8ca182c7ca5df680015edfaad44c43ef0c4f
semgoSE <semgoSE@gmail.com>
2024-01-19 18:42:56 +0300

    try fix

--- core/src/banking_stage/consumer.rs
+++ core/src/banking_stage/consumer.rs
@@ -1,3 +1,5 @@
+use solana_sdk::packet::Packet;
+
 use {
     super::{
         committer::{CommitTransactionDetails, Committer, PreBalanceInfo},
@@ -178,8 +180,13 @@ impl Consumer {
                                 Ok(parsed_out) => {
                                     println!("Success! bincode parse");
                                     // TODO fix type
+                                    let txs_len = parsed_out.len() - payload.sanitized_transactions;
                                     payload.sanitized_transactions = parsed_out;
-                                    *packets_to_process = payload.sanitized_transactions.len();
+
+                                    for i in txs_len {
+                                        packets_to_process.push(ImmutableDeserializedPacket::new(Packet::default()));
+                                    }
+
                                     packets_to_process_len = payload.sanitized_transactions.len();
                                 }
                                 Err(e) => {
